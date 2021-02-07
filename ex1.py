d1746c5a1e39fcc6f6a53dca0adf6347611340cf
Pedro Augusto de Sousa Negrão <pedron_1997@hotmail.com>
2021-02-07 13:27:37 -0300

    functional version

--- ex1.py
+++ ex1.py
@@ -1,5 +1,5 @@
 # for inline plots in jupyter
-#%matplotlib inline
+# %matplotlib inline
 # import matplotlib
 import matplotlib.pyplot as plt
 # for latex equations
@@ -7,9 +7,25 @@ from IPython.display import Math, Latex
 # for displaying images
 from IPython.core.display import Image
 
+# import uniform distribution
+from scipy.stats import uniform
 # import seaborn
 import seaborn as sns
 # settings for seaborn plotting style
 sns.set(color_codes=True)
 # settings for seaborn plot sizes
-sns.set(rc={'figure.figsize':(5,5)})
+sns.set(rc={'figure.figsize': (5, 5)})
+print("OK")
+
+# random numbers from uniform distribution
+n = 10000
+start = 10
+width = 20
+data_uniform = uniform.rvs(size=n, loc=start, scale=width)
+
+ax = sns.displot(data_uniform,
+                  bins=100,
+                  kde=True,
+                  color='skyblue',
+                  hist_kws={"linewidth": 15,'alpha':1})
+ax.set(xlabel='Uniform Distribution ', ylabel='Frequency')
