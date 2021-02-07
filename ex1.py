3d026381524456d31b96cadaddc65217408df741
Pedro Augusto de Sousa Negrão <pedron_1997@hotmail.com>
2021-02-07 12:38:45 -0300

    initial commit

---
+++ ex1.py
@@ -0,0 +1,15 @@
+# for inline plots in jupyter
+#%matplotlib inline
+# import matplotlib
+import matplotlib.pyplot as plt
+# for latex equations
+from IPython.display import Math, Latex
+# for displaying images
+from IPython.core.display import Image
+
+# import seaborn
+import seaborn as sns
+# settings for seaborn plotting style
+sns.set(color_codes=True)
+# settings for seaborn plot sizes
+sns.set(rc={'figure.figsize':(5,5)})
