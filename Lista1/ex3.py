5766c7fa7d0788a0ed0792e3c4bda584288ed1cf
Pedro Augusto de Sousa Negrão <pedron_1997@hotmail.com>
2021-02-11 16:31:47 -0300

    Update ex3.py

--- Lista1/ex3.py
+++ Lista1/ex3.py
@@ -1,6 +1,8 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import statistics
 
 # Set your path to the folder containing the .csv files
 PATH = './Data/' # Use your path
@@ -14,15 +16,15 @@ fileNames = [file for file in fileNames if '.csv' in file]
 IPCA = pd.DataFrame(pd.read_csv(PATH + fileNames[0]))
 Desemprego = pd.DataFrame(pd.read_csv(PATH + fileNames[1]))
 
-Desemprego = Desemprego.set_index("Data")
 IPCA = IPCA.set_index("Data")
+Desemprego = Desemprego.set_index("Data")
 
-print(Desemprego.head())
-print(IPCA.head())
+#print(Desemprego.head())
+#print(IPCA.head())
 
 df3 = pd.merge(IPCA, Desemprego, left_index=True, right_index=True)
 df3 = df3[[' IPCA ','Taxa de desemprego ']]
-print(df3.head())
+#print(df3.head())
 
 plt.plot(df3.iloc[:, 0], label="IPCA")
 plt.plot(df3.iloc[:, 1], label="Tx. Desemprego")
@@ -30,7 +32,16 @@ plt.plot(df3.iloc[:, 1], label="Tx. Desemprego")
 
 #print(df3.iloc[:, 0])
 plt.legend()
-plt.show()
+#plt.show()
+
+print("IPCA Mean: ", np.mean(df3.iloc[:, 0]))
+print("IPCA Variance: ", np.var(df3.iloc[:, 0]))
+print("IPCA Standard Deviation: ", np.std(df3.iloc[:, 0]))
+
+#print("IPCA Covariance: ", np.cov(df3))
+
+print("Covariance between IPCA and Taxa de desemprego: : ", np.cov(df3.iloc[:, 0], df3.iloc[:, 1]))
+print("Correlation Coefficient between IPCA and Taxa de desemprego: ", np.corrcoef(df3.iloc[:, 0], df3.iloc[:, 1]))
 
 #Desemprego.iloc[0,0]
 """
