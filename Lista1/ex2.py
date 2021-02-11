0ef8a3a2405f60ce49660d342ff4801cc4417d03
Pedro Augusto de Sousa Negrão <pedron_1997@hotmail.com>
2021-02-11 16:34:49 -0300

    Update ex2.py

--- Lista1/ex2.py
+++ Lista1/ex2.py
@@ -9,8 +9,7 @@ rv = uniform(-2, 5)
 #xk = np.arange(5)
 xk = np.arange(start=-3, stop=4)
 
-pk = (0, 0.2*(-xk[1]), 0.2*(-xk[2]), 0.2*xk[3], 0.2*xk[4], 0.2*xk[5], 0)
-# pk = (0, 0.2, 0.2, 0.2, 0.2, 0.2, 0)
+pk = (0, 0.2, 0.2, 0.2, 0.2, 0.2, 0)
 custm = stats.rv_discrete(name='custm', values=(xk, pk))
 
 fig, ax = plt.subplots(1, 1)
